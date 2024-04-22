9b1e685e59fb5b958938d9a9bcde4af48d7480da
unknown <21522117@gm.uit.edu.vn>
2024-04-22 21:41:25 +0700

    add product entity

---
+++ src/main/java/com/example/demo/entity/Products.java
@@ -0,0 +1,159 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity(name="Products")
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int product_id;
+
+    @ManyToOne
+    @JoinColumn(name = "shoe_id")
+    private Shoes shoes;
+
+    @ManyToOne
+    @JoinColumn(name = "color_id")
+    private Colors colors;
+
+    @ManyToOne
+    @JoinColumn(name = "size_id")
+    private Sizes sizes;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Categories categories;
+
+    @ManyToOne
+    @JoinColumn(name = "gender_id")
+    private Genders genders;
+
+    @ManyToOne
+    @JoinColumn(name = "material_id")
+    private Materials materials;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private Types types;
+
+    @Column(name="quantity")
+    private int quantity;
+
+    @Column(name="image_url")
+    private String image_url;
+
+    @Column(name="discount")
+    private int discount;
+
+    public int getproduct_id() {
+        return product_id;
+    }
+
+    public void setproduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public Shoes getShoes() {
+        return shoes;
+    }
+
+    public void setShoes(Shoes shoes) {
+        this.shoes = shoes;
+    }
+
+    public Colors getColors() {
+        return colors;
+    }
+
+    public void setColors(Colors colors) {
+        this.colors = colors;
+    }
+
+    public Sizes getSizes() {
+        return sizes;
+    }
+
+    public void setSizes(Sizes sizes) {
+        this.sizes = sizes;
+    }
+
+    public Categories getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Categories categories) {
+        this.categories = categories;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getImage() {
+        return image_url;
+    }
+
+    public void setImage(String image) {
+        this.image_url = image;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public Genders getGenders() {
+        return genders;
+    }
+
+    public void setGenders(Genders genders) {
+        this.genders = genders;
+    }
+
+    public Materials getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(Materials materials) {
+        this.materials = materials;
+    }
+
+    public Types getTypes() {
+        return types;
+    }
+
+    public void setTypes(Types types) {
+        this.types = types;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+}
