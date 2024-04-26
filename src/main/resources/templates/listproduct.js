7109c1da969c19ae1ac462499b8b2ce4afd61d19
unknown <21522117@gm.uit.edu.vn>
2024-04-26 13:40:30 +0700

    fix html

--- src/main/resources/templates/listproduct.js
+++ src/main/resources/templates/listproduct.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    var link_product = "http://localhost:8080/product/allproduct"
-    $.ajax({
-        method: "GET",
-        url: link_product
+  var link_product = "http://localhost:8080/product/allproduct"
+  $.ajax({
+    method: "GET",
+    url: link_product
 
-    })
-        .done(function (msg) {
-            if (msg) {
-                $.each(msg.data, function (index, value) {
-                    console.log(value);
-                    var html = `<div class="san-pham">
+  })
+    .done(function (msg) {
+      if (msg) {
+        $.each(msg.data, function (index, value) {
+          console.log(value);
+          var html = `<div class="san-pham">
                     <div class="container-hover-image">
                       <a href=""><img class="rectangle-38" img src="${value.image_url}" alt=""></a>
                       <div class="button-hover"><a class="mua-ngay" href="#"> MUA NGAY </a></div>
@@ -22,76 +22,73 @@ $(document).ready(function () {
                     </span>
                   </div>`
 
-                    $("#container-san-pham").append(html)
-                })
-            }
-
-        });
-
-        
-    $.ajax({
-        method: "GET",
-        url: "http://localhost:8080/product/allstylename"
-
-    })
-        .done(function (msg) {
-            if (msg) {
-                $.each(msg.data, function (index, value) {
-                    console.log(value);
-                    var html = ` <div class="component-11">
-                    <span class="limited-edition-1">
-                      ${value}
-                    </span>
-                  </div>`
-
-                    $("#kieu-dang").append(html)
-                })
-            }
-
-        });
-        
-        $.ajax({
-          method: "GET",
-          url: "http://localhost:8080/product/allcategoryname"
-  
-      })
-          .done(function (msg) {
-              if (msg) {
-                  $.each(msg.data, function (index, value) {
-                      console.log(value);
-                      var html = ` <div class="component-11">
-                      <span class="limited-edition-1">
-                        ${value}
-                      </span>
-                    </div>`
-  
-                      $("#dong-san-pham").append(html)
-                  })
-              }
-  
-          });
-
-          $.ajax({
-            method: "GET",
-            url: "http://localhost:8080/product/allmaterialname"
-    
+          $("#container-san-pham").append(html)
         })
-            .done(function (msg) {
-                if (msg) {
-                    $.each(msg.data, function (index, value) {
-                        console.log(value);
-                        var html = ` <div class="component-11">
-                        <span class="limited-edition-1">
-                          ${value}
-                        </span>
-                      </div>`
-    
-                        $("#chat-lieu").append(html)
-                    })
-                }
-    
-            });
-  
+      }
+
+    });
+
+
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:8080/product/allstylename"
+
+  })
+    .done(function (msg) {
+      if (msg) {
+        $.each(msg.data, function (index, value) {
+          console.log(value);
+          var html = ` <input type="checkbox" class="myinputCheckbox" id=" ${value}">
+                    <label for=" ${value}">
+                        <span class="text"> ${value}</span>
+                    </label>`
+
+          $("#kieu-dang").append(html)
+        })
+      }
+
+    });
+
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:8080/product/allcategoryname"
+
+  })
+    .done(function (msg) {
+      if (msg) {
+        $.each(msg.data, function (index, value) {
+          console.log(value);
+          var html = ` <input type="checkbox" class="myinputCheckbox" id=" ${value}">
+          <label for=" ${value}">
+              <span class="text"> ${value}</span>
+          </label>`
+
+          $("#dong-san-pham").append(html)
+        })
+      }
+
+    });
+
+  $.ajax({
+    method: "GET",
+    url: "http://localhost:8080/product/allmaterialname"
+
+  })
+    .done(function (msg) {
+      if (msg) {
+        $.each(msg.data, function (index, value) {
+          console.log(value);
+          var html = ` <input type="checkbox" class="myinputCheckbox" id=" ${value}">
+          <label for=" ${value}">
+              <span class="text"> ${value}</span>
+          </label>`
+
+          $("#chat-lieu").append(html)
+        })
+      }
+
+    });
+
 
 
 })
