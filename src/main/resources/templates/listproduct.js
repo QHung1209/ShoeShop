4fb113106abe9dea858b14d84b2d3fc7c0f12fe6
unknown <21522117@gm.uit.edu.vn>
2024-05-02 14:33:39 +0700

    add param checkbox to url

--- src/main/resources/templates/listproduct.js
+++ src/main/resources/templates/listproduct.js
@@ -1,3 +1,57 @@
+function updateURL() {
+  var checkedCheckboxes = document.querySelectorAll('.myinputCheckbox:checked');
+  var queryParams = [];
+  checkedCheckboxes.forEach(function (checkbox) {
+    queryParams.push(checkbox.id);
+  });
+
+  var checkedCheckboxes_kieudang = document.querySelectorAll('.kieu-dang:checked');
+  var queryParams_kieudang = [];
+  checkedCheckboxes_kieudang.forEach(function (checkbox) {
+    queryParams_kieudang.push(checkbox.id);
+  });
+
+  var checkedCheckboxes_dongsanpham = document.querySelectorAll('.dong-san-pham:checked');
+  var queryParams_dongsanpham = [];
+  checkedCheckboxes_dongsanpham.forEach(function (checkbox) {
+    queryParams_dongsanpham.push(checkbox.id);
+  });
+
+  // Get base URL
+  var url = window.location.href.split('?')[0];
+
+  // Additional parameters such as gender and category
+  if (queryParams.length > 0 || queryParams_kieudang.length > 0 || queryParams_dongsanpham.length > 0) {
+    url += '?'; // Add '?' if it doesn't exist
+  }
+
+  if (queryParams.length > 0) {
+    url += 'price=';
+    url += queryParams.join(',');
+  }
+
+  if (queryParams_kieudang.length > 0 ) {
+    if (queryParams.length > 0 ) {
+      url += '&styles='; // Add '&' to append to existing params
+    }
+    else
+      url += 'styles=';
+    url += queryParams_kieudang.join(',');
+  }
+
+  if (queryParams_dongsanpham.length > 0) {
+    if (queryParams.length > 0 || queryParams_kieudang.length > 0) {
+      url += '&categories='; // Add '&' to append to existing params
+    }
+    else
+      url += 'categories=';
+    url += queryParams_dongsanpham.join(',');
+  }
+
+  window.history.replaceState({}, document.title, url);
+}
+
+
 $(document).ready(function () {
   var link_product = "http://localhost:8080/product/allproduct"
   $.ajax({
@@ -38,15 +92,15 @@ $(document).ready(function () {
       if (msg) {
         $.each(msg.data, function (index, value) {
           console.log(value);
-          var html = ` <input type="checkbox" class="myinputCheckbox" id=" ${value}">
-                    <label for=" ${value}">
+          var html = ` <input type="checkbox" class="kieu-dang" id="${value}" name=" ${value}">
+                    <label for="${value}">
                         <span class="text"> ${value}</span>
                     </label>`
 
           $("#kieu-dang").append(html)
         })
       }
-
+      updateURL();
     });
 
   $.ajax({
@@ -58,15 +112,15 @@ $(document).ready(function () {
       if (msg) {
         $.each(msg.data, function (index, value) {
           console.log(value);
-          var html = ` <input type="checkbox" class="myinputCheckbox" id=" ${value}">
-          <label for=" ${value}">
+          var html = ` <input type="checkbox" class="dong-san-pham" id="${value}" name=" ${value}">
+          <label for="${value}">
               <span class="text"> ${value}</span>
           </label>`
 
           $("#dong-san-pham").append(html)
         })
       }
-
+      updateURL();
     });
 
   $.ajax({
@@ -78,17 +132,20 @@ $(document).ready(function () {
       if (msg) {
         $.each(msg.data, function (index, value) {
           console.log(value);
-          var html = ` <input type="checkbox" class="myinputCheckbox" id=" ${value}">
-          <label for=" ${value}">
+          var html = ` <input type="checkbox" class="myinputCheckbox" id="${value}" name=" ${value}">
+          <label for="${value}">
               <span class="text"> ${value}</span>
           </label>`
 
           $("#chat-lieu").append(html)
         })
       }
-
+      updateURL();
     });
 
+  $(document).on('change', '.myinputCheckbox', updateURL);
+  $(document).on('change', '.kieu-dang', updateURL);
+  $(document).on('change', '.dong-san-pham', updateURL);
 
 
 })
