092d9cf33551d1dfff95a47b575b7897cfad0d33
unknown <21522117@gm.uit.edu.vn>
2024-05-25 22:29:11 +0700

    add size_id to orderdetail

--- src/main/java/com/example/demo/service/OrderDetailService.java
+++ src/main/java/com/example/demo/service/OrderDetailService.java
@@ -1,11 +1,13 @@
 package com.example.demo.service;
 
+import org.hibernate.engine.jdbc.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Order_detail;
 import com.example.demo.entity.Orders;
 import com.example.demo.entity.Products;
+import com.example.demo.entity.Sizes;
 import com.example.demo.repository.OrderDetailRepository;
 import com.example.demo.service.imp.OrderDetailServiceImp;
 
@@ -15,14 +17,17 @@ public class OrderDetailService implements OrderDetailServiceImp {
     @Autowired
     OrderDetailRepository orderDetailRepository;
     @Override
-    public boolean insertOrderDetail(int order_id, int product_id, int quantity, int price) {
+    public boolean insertOrderDetail(int order_id, int product_id, int size_id, int quantity, int price) {
         Order_detail temp = new Order_detail();
         Orders orders = new Orders();
         Products products = new Products();
+        Sizes size = new Sizes();
+        size.setSize_id(size_id);
         orders.setOrder_id(order_id);
         products.setProduct_id(product_id);
         temp.setOrders(orders);
         temp.setProducts(products);
+        temp.setSizes(size);
         temp.setQuantity(quantity);
         temp.setPrice(price);
         orderDetailRepository.save(temp);
